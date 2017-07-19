4500e49a00b1675c2c75f9d7cbecaf2276c7f913
Dave Larson <dlarson@genome.wustl.edu>
2017-07-19 12:47:36 -0500

    retrieve timestamp from file

--- pipeinspector/utils.py
+++ pipeinspector/utils.py
@@ -1,4 +1,6 @@
 import hashlib
+import os.path
+import datetime
 
 def md5_checksum(file_name):
     hash_md5 = hashlib.md5()
@@ -6,3 +8,13 @@ def md5_checksum(file_name):
         for chunk in iter(lambda: f.read(4096), b''):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
+
+def read_first_checksum(file_name):
+    with open(file_name, 'r') as f:
+        for line in f:
+            checksum, filepath = line.rstrip().split('  ')
+            return checksum
+
+def file_mtime(path):
+    mtime = os.path.getmtime(path)
+    return datetime.datetime.utcfromtimestamp(mtime)
