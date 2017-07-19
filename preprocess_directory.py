ce8eee26aac5e7919ed141ea0a267d161d83c1b5
Dave Larson <dlarson@genome.wustl.edu>
2017-07-19 12:47:14 -0500

    do CRAM too

--- preprocess_directory.py
+++ preprocess_directory.py
@@ -3,6 +3,7 @@
 import sys
 import json
 import os
+import os.path
 import errno
 import argparse
 
@@ -64,6 +65,26 @@ class B38Preprocessor(object):
             except OSError as e:
                 if e.errno != errno.EEXIST: #don't freak out if the directory exists
                     raise
+            
+            stdout_dir = os.path.join(self.logdir, d.sample_name())
+            try:
+                os.makedirs(stdout_dir)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+
+            # always submit a CRAM transfer because we use rsync and it checks these things...
+            copy_stdout = os.path.join(stdout_dir, 'cram_copy.log')
+            cram_shortcutter = Shortcutter(d, outdir, '.cram_file_md5s.json', lambda x: x.cram_files())
+            cram, crai = d.cram_files()
+            new_cram = os.path.basename(cram)
+            output_cram = os.path.join(outdir, new_cram)
+            output_crai = output_cram + '.crai'
+            if not (cram_shortcutter.can_shortcut(cram, output_cram) and cram_shortcutter.can_shortcut(crai, output_crai)):
+                cram_copy_cmd = RsyncCmd()
+                cram_copy_cmdline = cram_copy_cmd(d.cram_file(), outdir)
+                self.lsf_job_runner.launch(cram_copy_cmdline, { 'stdout': copy_stdout })
+
             shortcutter = GVCFShortcutter(d, outdir)
             for gvcf in d.all_gvcf_files():
                 new_gzvcf = os.path.basename(gvcf)
@@ -78,20 +99,15 @@ class B38Preprocessor(object):
                             break_multiple=1000000
                             )
                     cmdline = cmd(gvcf, output_gzvcf)
-                    stdout_dir = os.path.join(self.logdir, d.sample_name())
-                    try:
-                        os.makedirs(stdout_dir)
-                    except OSError as e:
-                        if e.errno != errno.EEXIST:
-                            raise
                     stdout = os.path.join(stdout_dir, new_gzvcf + '.log')
                     lsf_options = {
                             'stdout': stdout,
                             }
-                    print self.lsf_job_runner.dry_run(cmdline, lsf_options) 
                     self.lsf_job_runner.launch(cmdline, lsf_options)
+            return outdir
         else:
             print 'Invalid for processing'
+            return None
 
     def output_directory(self, d):
         return os.path.join(self.dest_dir, d.sample_name())
@@ -152,11 +168,66 @@ class RewriteGvcfCmd(object):
         temp_output = output_file + '.tmp.vcf.gz'
         return self.cmd.format(input=input_file, output=output_file, temp_output=temp_output)
 
+class Shortcutter(object):
+    def __init__(self, input_directory, output_directory, json_name, file_path_func):
+        self.output_directory = output_directory
+        self.input_directory = input_directory
+        self.json_name = json_name
+        self.file_path_func = file_path_func
+        self._input_md5s = self._calc_input_md5s()
+        self._output_md5s = self._calc_output_md5s()
+
+    # NOTE Md5 keys should be basename, not full path
+    def _md5_json(self):
+        return os.path.join(self.output_directory, self.json_name)
+        
+    def _calc_input_md5s(self):
+        md5s = dict()
+        #filepaths = (self.input_directory.cram_file(), self.input_directory.cram_file() + '.crai')
+        for filepath in self.file_path_func(self.input_directory):
+            filename = os.path.basename(filepath)
+            if os.path.isfile(filepath + '.md5'):
+                # trust the md5 file to save compute
+                md5s[filename] = utils.read_first_checksum(filepath + '.md5')
+            else:
+                md5s[filename] = utils.md5_checksum(filepath)
+        return md5s
+
+    def _calc_output_md5s(self):
+        json_file = self._md5_json()
+        if os.path.isfile(json_file):
+            with open(json_file) as jsonfile:
+                return json.load(jsonfile)
+        else:
+            md5s = self._calc_input_md5s()
+            with open(json_file, 'w') as output:
+                json.dump(md5s, output)
+            return md5s
+    
+    def can_shortcut(self, input_file, output_file):
+        filename = os.path.basename(input_file)
+        input_md5 = self._input_md5s[filename]
+        try:
+            output_md5 = self._output_md5s[filename]
+        except KeyError:
+            raise RuntimeError('{0} not previously recorded in {1} md5 json. This is likely due to a code change in lims or here.'.format(input_file, self.output_directory))
+        if os.path.isfile(output_file) and input_md5 == output_md5:
+            return True
+        else:
+            return False
+
+class RsyncCmd(object):
+    def __init__(self):
+        self.cmd = 'rsync --verbose --archive {input} {output_dir}/ && rsync --verbose --archive {input}.md5 {output_dir}/ && rsync --verbose --archive {input}.crai {output_dir}/'
+    def __call__(self, input_file, output_dir):
+        return self.cmd.format(input=input_file, output_dir=output_dir)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Pre-process LIMS build38 realignment directory gvcfs', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('directories', metavar='<DIR>', nargs='+', help='LIMS output directories to process')
     parser.add_argument('--output-dir', metavar='<DIR>', default='/gscmnt/gc2758/analysis/ccdg/data', help='output directory to place processed gvcfs within a directory for the sample.')
     parser.add_argument('--job-group', metavar='<STR>', help='LSF job group for submitted jobs')
+    #parser.add_argument('--dry-run', dest='dry_run', action='store_true', help='dry run, no jobs submitted')
     args = parser.parse_args()
     default_job_options = {
         'memory_in_gb': 5,
