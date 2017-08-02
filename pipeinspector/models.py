6c3f6758e402270e0f49e7f78b3b780ec90d2c7d
Dave Larson <dlarson@genome.wustl.edu>
2017-08-01 23:33:17 -0500

    formatting clean up

--- pipeinspector/models.py
+++ pipeinspector/models.py
@@ -1,21 +1,25 @@
 from sqlalchemy import Column, Text, Integer, Boolean, DateTime, UniqueConstraint
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
 
 Base = declarative_base()
 
+
 class ComputeWorkflowSample(Base):
-    
+
     __tablename__ = 'csp_sample'
     __table_args__ = (
-            UniqueConstraint('source_work_order', 'woi', 'source_directory', name='uniq_row'),
+            UniqueConstraint(
+                'source_work_order',
+                'woi',
+                'source_directory',
+                name='uniq_row'),
             )
-    
+
     id = Column(Integer, primary_key=True)
     source_work_order = Column(Integer)
     woi = Column(Integer)
-    ingest_sample_name = Column(Text, unique=True, nullable=False) #convenience. Could change in the LIMS 
+    ingest_sample_name = Column(Text, unique=True, nullable=False)
     source_directory = Column(Text, unique=True, nullable=False)
     valid_source_directory = Column(Boolean)
     passed_qc = Column(Boolean)
@@ -26,4 +30,3 @@ class ComputeWorkflowSample(Base):
     analysis_sv_path = Column(Text)
     analysis_sv_verified = Column(Boolean)
     ingest_date = Column(DateTime, default=datetime.datetime.utcnow)
-
