29a5e42217c1520cf016d1b30319e2871ab72647
Dave Larson <dlarson@genome.wustl.edu>
2017-08-02 08:43:01 -0500

    Whitespace changes to appease Flake8

--- pipeinspector/utils.py
+++ pipeinspector/utils.py
@@ -2,6 +2,7 @@ import hashlib
 import os.path
 import datetime
 
+
 def md5_checksum(file_name):
     hash_md5 = hashlib.md5()
     with open(file_name, 'rb') as f:
@@ -9,11 +10,13 @@ def md5_checksum(file_name):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
+
 def read_first_checksum(file_name):
     with open(file_name, 'r') as f:
         for line in f:
             checksum, filepath = line.rstrip().split('  ')
             return checksum
+
 
 def file_mtime(path):
     mtime = os.path.getmtime(path)
