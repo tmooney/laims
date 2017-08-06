dfe2bf5ed0a71a13e9322e83d8c62e68a4ef6f03
Dave Larson <dlarson@genome.wustl.edu>
2017-08-05 22:43:06 -0500

    Minor whitespace cleanup.

--- pipeinspector/directoryvalidation.py
+++ pipeinspector/directoryvalidation.py
@@ -1,6 +1,7 @@
 import glob
 import os.path
 
+
 class DirectoryValidator(object):
 
     def __init__(self, expectations):
@@ -21,11 +22,13 @@ class DirectoryValidator(object):
         if output_file_dict is None:
             output_file_dict = self.collect_output_file_dict(directory)
         complete = True
-        reasons = [ '{0} is not complete'.format(directory) ]
+        reasons = ['{0} is not complete'.format(directory)]
         for glob_string, num_expected in self.expectations.iteritems():
             if not num_expected == len(output_file_dict[glob_string]):
                 complete = False
-                reasons.append('\tMissing files matching {0}'.format(glob_string))
+                reasons.append(
+                    '\tMissing files matching {0}'.format(glob_string)
+                )
             for file_ in output_file_dict[glob_string]:
                 if os.path.getsize(file_) == 0:
                     complete = False
@@ -38,4 +41,6 @@ class DirectoryValidator(object):
             complete = self.complete(directory)
         if output_file_dict is None:
             output_file_dict = self.collect_output_file_dict(directory)
-        return max([os.path.getmtime(x) for sublist in output_file_dict.itervalues() for x in sublist])
+        return max([os.path.getmtime(x)
+                    for sublist in output_file_dict.itervalues()
+                    for x in sublist])
