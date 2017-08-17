92d9f675b327a22ca0728d3b154e8fecb6913fb6
Dave Larson <dlarson@genome.wustl.edu>
2017-08-17 13:52:56 -0500

    modify since database switch

--- pipeinspector/limsdatabase.py
+++ pipeinspector/limsdatabase.py
@@ -3,7 +3,7 @@ import csv
 
 class ReadCountInDb(object):
     def __init__(self):
-        self._sql = 'sqlrun "select seq_id, filt_clusters * 2 from index_illumina where seq_id in ({0})" --instance warehouse --parse'
+        self._sql = 'sqlrun "select seq_id, filt_clusters * 2 from index_illumina where seq_id in ({0})" --parse'
 
     def __call__(self, ids):
         total = 0
@@ -11,5 +11,5 @@ class ReadCountInDb(object):
         output = os.popen(self._sql.format(id_string))
         reader = csv.reader(output, delimiter='\t')
         for row in reader:
-            total += int(row[1])
+            total += int(float(row[1]))
         return total
