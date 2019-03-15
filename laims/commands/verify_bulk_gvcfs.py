36ec7f72dc7b865db840d9053ecb749366592f33
Eddie Belter <ebelter@gmail.com>
2019-03-15 13:31:38 -0500

    Verify Bulk GVCFs
    use given queue
    misc code fixes

--- laims/commands/verify_bulk_gvcfs.py
+++ laims/commands/verify_bulk_gvcfs.py
@@ -6,13 +6,13 @@ import subprocess
 
 from laims.lsf import LsfJob
 
-def verify_bulk_gvcfs(tsv_path, reference_path):
+def verify_bulk_gvcfs(app, tsv_path, reference_path):
 
     os.environ['LSF_DOCKER_PRESERVE_ENVIRONMENT'] = 'false'
     job_opts = {
         'memory_in_gb': 10,
-        'queue': 'ccdg',
-        'docker': 'broadinstitute/gatk:4.0.6.0',
+        'queue': app.queue,
+        'docker': 'registry.gsc.wustl.edu/ebelter/laims:latest',
         'group': '/apipe-builder/verify-gvcf',
         'email': 'ebelter@wustl.edu',
     }
@@ -22,7 +22,7 @@ def verify_bulk_gvcfs(tsv_path, reference_path):
         reader = csv.reader(f, delimiter='\t')
         for row in reader:
             interval = get_interval_from_path(row[0])
-            cmd = [ "laims", "verify-gvcf", "--gvcf-path", row[0], "--reference", reference_path, "--interval", interval ]
+            cmd = [ "laims", "verify-gvcf", "--gvcf-path", row[0], "--reference-path", reference_path, "--interval", interval ]
             #print(job_runner.dry_run(cmd, {}))
             job_runner.launch(cmd, {})
 
@@ -31,7 +31,7 @@ chromosomes = [ 'chr{}'.format(c) for c in range(1,23) ]
 chromosomes.extend(['chrX', 'chrY'])
 def get_interval_from_path(path):
     for t in os.path.basename(path).split('.'):
+        if t == 'extChr': return "/gscmnt/gc2802/halllab/ccdg_resources/genomes/human/GRCh38DH/extChr.list"
         for c in chromosomes:
             if t == c: return c
-    raise 'Failed to get interval from file name: {}'.foramt(path)
-
+    raise 'Failed to get interval from file name: {}'.format(path)
