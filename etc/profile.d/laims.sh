c793f4bfdd65a1a82a59acb32f8f4051c024d1bb
Eddie Belter <ebelter@gmail.com>
2019-03-18 14:07:47 -0500

    Do not check host when setting HOME

--- etc/profile.d/laims.sh
+++ etc/profile.d/laims.sh
@@ -1,21 +1,14 @@
 # laims mgi lsf -*-sh-*-
 
-host=$(hostname)
-if [[ "${host}" == *.gsc.wustl.edu ]]; then # @ MGI
-    # LSF
-    if hash bsub 2> /dev/null; then 
-        [ -f /opt/lsf9/conf/profile.lsf ] && . /opt/lsf9/conf/profile.lsf
-    fi
+# LSF
+[ -f /opt/lsf9/conf/profile.lsf ] && . /opt/lsf9/conf/profile.lsf
 
-    # Set Home
-    iam=$(whoami)
-    if [ -d "/gscmnt/gc2802/halllab/${iam}" ]; then
-        HOME="/gscmnt/gc2802/halllab/${iam}"
-    elif [ -d "/gscuser/${iam}" ]; then
-        HOME="/gscuser/${iam}"
-    fi
-    export HOME
-    unset iam
-
+# Set Home
+iam=$(whoami)
+if [ -d "/gscmnt/gc2802/halllab/${iam}" ]; then
+    HOME="/gscmnt/gc2802/halllab/${iam}"
+elif [ -d "/gscuser/${iam}" ]; then
+    HOME="/gscuser/${iam}"
 fi
-unset host
+export HOME
+unset iam
