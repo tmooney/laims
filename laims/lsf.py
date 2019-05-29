ce776efe254ed1ff7eda7994fc7e73a14713ea29
kkanchi <kkanchi@genome.wustl.edu>
2019-05-29 15:31:49 -0500

    + correctly concatentate LSF commands

--- laims/lsf.py
+++ laims/lsf.py
@@ -73,7 +73,7 @@ class LsfJob(object):
 
     def _construct_cmd(self, cmd, cmd_options):
         bsub_cmd = self._construct_bsub(cmd_options)
-        return bsub_cmd + cmd
+        return bsub_cmd + cmd.split(' ')
 
     def bsub_cmd(self, cmd, cmd_options):
         return ' '.join(self._construct_cmd(cmd, cmd_options))
