aeeb25570c704c648c1b0f0a459643da6657db2e
Eddie Belter <ebelter@gmail.com>
2019-05-30 18:42:03 -0500

    Lims DB uses LaimsApp lims_db_connection

--- laims/limsdatabase.py
+++ laims/limsdatabase.py
@@ -1,24 +1,14 @@
-import os
-import csv
-
-import dataset
+import csv, os
 from jinja2 import Template
+
+from laims.app import LaimsApp
 
 class ReadCountInDb(object):
     def __init__(self):
         self._sql = 'sqlrun "select seq_id, filt_clusters * 2 from index_illumina where seq_id in ({0})" --parse'
 
-    #psql -U gscguest -h lims-db.gsc.wustl.edu lims
-    def get_lims_database_connection(self):
-        user = 'gscguest'
-        password = app.get_lims_pwd ##TODO fix this
-        server = 'lims-db.gsc.wustl.edu'
-        database = 'lims'
-        url = 'postgresql://{0}:{1}@{2}/{3}'.format(user, password, server, database)
-        db = dataset.connect(url, schema='gsc')
-        return db
-
     def get_read_counts(self, read_groups):
+        #psql -U gscguest -h lims-db.gsc.wustl.edu lims
         # select seq_id, filt_clusters * 2 from index_illumina where seq_id in ({0})" --parse
         sql = """
             select ii.seq_id, ii.filt_clusters * 2 as total_clusters
@@ -28,7 +18,7 @@ class ReadCountInDb(object):
 
         template = Template(sql)
         rendered_sql = template.render(seq_ids=read_groups)
-        db_connection = self.get_lims_database_connection()
+        db_connection = LaimsApp().lims_database_connection()
         result = db_connection.query(rendered_sql)
         data = [ { 'seq_id' : row['seq_id'],
                    'filt_clusters' : row['total_clusters'] } for row in result ]
