8f882d722811b8c53c6dd81947eeb2ffa855dcc0
Eddie Belter <ebelter@gmail.com>
2019-05-30 14:50:16 -0500

    LAIMS App Singleton

---
+++ laims/app.py
@@ -0,0 +1,48 @@
+import click, json, os
+from logzero import logger
+
+class LaimsApp(object):
+    context = None
+    class __LaimsAppSingleton(object):
+        def __init__(self, config_file, config):
+            config2 = {}
+            if config_file:
+                if not os.path.exists(config_file):
+                    raise Exception("Given config file {} does not exist!".format(config_file))
+                config2 = LaimsApp.load_config(config_file)
+            elif config_file is None:
+                config_file = os.path.join(click.get_app_dir('laims'), 'config.json')
+                if os.path.exists(config_file):
+                    config2 = LaimsApp.load_config(config_file)
+            for key in config2:
+                if key in config and config[key] is not None: continue
+                config[key] = config2[key]
+            self.config = config
+
+#-- __LaimsAppSingleton            
+
+    def __init__(self, config_file=None, config={}):
+        if LaimsApp.context: raise Exception("Attempting to reinitialize LAIMS App!")
+        LaimsApp.context = LaimsApp.__LaimsAppSingleton(config_file=config_file, config=config)
+
+    def __getattr__(self, name):
+        if name in LaimsApp.context.config:
+            return LaimsApp.context.config[name]
+        return None
+
+    @staticmethod
+    def load_config(config_file):
+        if not os.path.exists(config_file):
+            logger.error('No configuration file found at {0}'.format(self.config_file))
+            return
+        config = None
+        with open(config_file) as f:
+            config = json.load(f)
+        config["config_file"] = config_file
+        return config    
+
+    def log_config(self):
+        logger.info('Using config at {0}'.format(self.config_file))
+        logger.info('Using database at {0}'.format(self.database))
+
+#-- LaimsApp
---
+++ tests/test_app.py
@@ -0,0 +1,36 @@
+import os, unittest
+
+from .context import laims
+from laims.app import LaimsApp
+
+class LaimsAppTest(unittest.TestCase):
+
+    def test1_init_fails(self):
+        with self.assertRaisesRegexp(Exception, "Given config file /laims.json does not exist!") as cm:
+            LaimsApp(config_file="/laims.json")
+
+    def test2_init(self):
+        # init w/o config
+        laimsapp = LaimsApp()
+        self.assertIsNotNone(LaimsApp.context)
+
+        # init w/ config
+        LaimsApp.context = None # reset
+        laimsapp = LaimsApp(config_file=os.path.join("tests", "test_app", "laims.json"), config={"database": "NOTDB"})
+        self.assertIsNotNone(laimsapp)
+        self.assertIsNotNone(laimsapp.context)
+        self.assertEqual(laimsapp.config_file, os.path.join("tests", "test_app", "laims.json"))
+        self.assertEqual(laimsapp.environment, 'test')
+        self.assertEqual(laimsapp.database, 'NOTDB')
+        self.assertIsNone(laimsapp.blah)
+     
+    def test3_reinit_fails(self):
+        with self.assertRaisesRegexp(Exception, "Attempting to reinitialize LAIMS App!"):
+            laimsapp = LaimsApp()
+
+# -- LaimsAppTest
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
+#-- __main__
