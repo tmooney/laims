3a46aaaca6796e54161880fa1eeb0e1662aa3358
Indraniel Das <idas@wustl.edu>
2019-06-04 14:00:15 -0500

    + correctly sort the list

--- laims/build38realignmentdirectory.py
+++ laims/build38realignmentdirectory.py
@@ -86,7 +86,7 @@ class Build38RealignmentDirectory(object):
 
     def _is_file_count_correct(self, glob_string, num_expected, input_files):
         if glob_string == '*verify_bam_id*':
-            file_basenames = [os.path.basename(i) for i in input_files].sort()
+            file_basenames = sorted([os.path.basename(i) for i in input_files])
             # TopMed verify_bam_id outputs have a 'GT' prefix on the '*.depthRG' files
             # CCDG verify_bam_id outputs do not have a 'GT' prefix on the '*.depthRG' files
             num_expected_files = num_expected[0] if file_basenames[0].startswith('GT') else num_expected[1]
