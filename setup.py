ff9f5bfa234ecde01db9cf81c0f5ba05b8772f3a
Indraniel Das <idas@wustl.edu>
2019-06-05 13:06:04 -0500

    + explicitly use a custom version of crimson
      - gotta do this until the version of crimson modified by @ernfrid is
        pulled into the "main" repository at https://github.com/bow/crimson
    
        See : https://github.com/bow/crimson/pull/4
    
        for more additional details

--- setup.py
+++ setup.py
@@ -9,13 +9,12 @@ setup(
     install_requires=[
         'click==6.7',
         'SQLAlchemy==1.1.11',
-        'Crimson>0.3.0',
         'logzero==1.3.1',
         'dataset==1.1.2',
         'psycopg2-binary==2.8.2',
-        'Jinja2==2.10.1'
+        'Jinja2==2.10.1',
+        'Crimson @ https://github.com/hall-lab/crimson/tarball/8d0791c129e0b7c138c5d26c82952a069c063c27'
     ],
-    dependency_links=['https://github.com/ernfrid/crimson/tarball/verifybamid#egg=Crimson-0.4.0'],
     entry_points={
             'console_scripts': [
             'laims=laims.cli:cli',
