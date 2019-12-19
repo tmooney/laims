653fe1b5d0a8e1814160c02d6c5cbe895e439ec7
Eddie Belter <ebelter@gmail.com>
2019-12-19 14:28:44 -0600

    Setup.py cleanup

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup, find_packages
 import versioneer
 
+tests_require = [
+    "mock",
+    "nose",
+]
 setup(
     name='laims',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
+    setup_requires=["pytest-runner"],
+    test_suite="nose.collector",
+    tests_require=tests_require,
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'click>=7.0',
-        'SQLAlchemy==1.1.11',
+        "SQLAlchemy>=1.3.10",
         'logzero==1.3.1',
         'dataset==1.1.2',
         'psycopg2-binary==2.8.2',
@@ -16,8 +23,6 @@ setup(
         'Crimson @ https://github.com/hall-lab/crimson/tarball/8d0791c129e0b7c138c5d26c82952a069c063c27'
     ],
     entry_points={
-            'console_scripts': [
-            'laims=laims.cli:cli',
-            ]
-        }
+        'console_scripts': [ 'laims=laims.cli:cli', ]
+    }
 )
