e8968d47e6f3901c970f08298ba7ad90e4a4a962
Eddie Belter <ebelter@gmail.com>
2019-12-26 19:22:01 -0600

    Sample update files

--- laims/sample_cli.py
+++ laims/sample_cli.py
@@ -1,10 +1,11 @@
-import click, tabulate, sys
+import click, os, tabulate, sys
 
 from laims.app import LaimsApp
 from laims.models import ComputeWorkflowSample
 
 # SAMPLE
 # list
+# update-files
 
 @click.group()
 def laims_sample_cli():
@@ -13,6 +14,7 @@ def laims_sample_cli():
     """
     pass
 
+# [list]
 @click.command()
 @click.option("--filter-by", "-f", required=False, help="Filter samples by workorder.")
 def sample_list_cmd(filter_by):
@@ -27,3 +29,29 @@ def sample_list_cmd(filter_by):
         rows += [map(str, [sample.id, sample.ingest_sample_name, sample.source_work_order])]
     sys.stdout.write( tabulate.tabulate(rows, ["ID", "NAME", "WORK_ORDER"], tablefmt="simple") )
 laims_sample_cli.add_command(sample_list_cmd, name="list")
+
+# [update-files]
+@click.command()
+@click.argument("file_type", type=click.Choice(["cram", "gvcf"]))
+@click.argument("fof")
+def sample_update_files_cmd(file_type, fof):
+    """
+    Update Samples Files
+
+    Give a file type, and an FOF of files to update. The sample name should be derivable from the filename.
+    """
+    attr = "_".join(["analysis", file_type, "path"])
+    sm = LaimsApp().db_connection()
+    session = sm()
+    with open(fof, "r") as f:
+        for fn in f.readlines():
+            sample_n = os.path.basename(fn).split(".")[0]
+            sample = session.query(ComputeWorkflowSample).filter_by(ingest_sample_name=sample_n).first()
+            if sample is None:
+                status = "NOT_FOUND"
+            else:
+                setattr(sample, attr, fn.rstrip())
+                status = getattr(sample, attr)
+            sys.stderr.write("{} {}\n".format(sample_n, status))
+    session.commit()
+laims_sample_cli.add_command(sample_update_files_cmd, name="update-files")
--- tests/test_sample_cli.py
+++ tests/test_sample_cli.py
@@ -1,13 +1,22 @@
-import os, unittest
+import os, shutil, tempfile, unittest
 from click.testing import CliRunner
 
 from laims.app import LaimsApp
-from laims.sample_cli import laims_sample_cli, sample_list_cmd
+from laims.sample_cli import laims_sample_cli, sample_list_cmd, sample_update_files_cmd
 
 class LaimsSampleCliTest(unittest.TestCase):
     def setUp(self):
-        self.config_fn = os.path.join(os.path.dirname(__file__), "data", "laims.json")
-        self.database_fn = os.path.join(os.path.dirname(__file__), "data", "test.db")
+        self.data_d = os.path.join(os.path.dirname(__file__), "data")
+
+        self.temp_d = tempfile.TemporaryDirectory()
+        self.database_fn = os.path.join(self.temp_d.name, "test.db")
+        shutil.copyfile(os.path.join(self.data_d, "test.db"), self.database_fn)
+
+        laimsapp = LaimsApp()
+        laimsapp.database = self.database_fn
+
+    def tearDown(self):
+        self.temp_d.cleanup()
 
     def test1_laims_sample_cli(self):
         runner = CliRunner()
@@ -19,23 +28,48 @@ class LaimsSampleCliTest(unittest.TestCase):
 
     def test2_laims_sample_list_cmd(self):
         runner = CliRunner()
-        laimsapp = LaimsApp(self.config_fn)
-        laimsapp.database = self.database_fn
 
         result = runner.invoke(sample_list_cmd, ["--help"])
         self.assertEqual(result.exit_code, 0)
 
         result = runner.invoke(sample_list_cmd, ["-f", "2854371"])
-        expected_output = """  ID  NAME                      WORK_ORDER
+        try:
+            self.assertEqual(result.exit_code, 0)
+            expected_output = """  ID  NAME                      WORK_ORDER
 ----  ----------------------  ------------
    1  H_XS-356091-0186761975       2854371
    2  H_XS-345839-0186762084       2854371
    3  H_XS-245227-0186760925       2854371
    4  H_XS-362472-0186761202       2854371
    5  H_XS-44610-0186760927        2854371"""
+            self.assertEqual(result.output, expected_output)
+        except:
+            print(result.output)
+            raise
+
+    def test3_laims_sample_update_files_cmd(self):
+        runner = CliRunner()
+        print(sample_update_files_cmd.name)
+
+        result = runner.invoke(sample_update_files_cmd, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+
+        samples = [ "H_XS-356091-0186761975", "H_XS-362472-0186761202", "H_WHEREAMI"]
+        new_crams = list(map(lambda s: os.path.join("/mnt/data/crams", s + ".cram"), samples))
+
+        with tempfile.NamedTemporaryFile(mode="w") as temp_f:
+            temp_f.write("\n".join(new_crams))
+            temp_f.flush()
+            result = runner.invoke(sample_update_files_cmd, ["cram", temp_f.name])
+
         try:
             self.assertEqual(result.exit_code, 0)
-            self.assertEqual(result.output, expected_output)
+            new_crams = list(new_crams)
+            expected_output = [ " ".join([samples[0], new_crams[0]]) ]
+            expected_output += [ " ".join([samples[1], new_crams[1]]) ]
+            expected_output += [ " ".join([samples[2], "NOT_FOUND"]) ]
+            expected_output += [""]
+            self.assertEqual(result.output, "\n".join(expected_output))
         except:
             print(result.output)
             raise
