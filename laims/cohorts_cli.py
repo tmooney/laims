e46bb72fadc978859d18941552519242ea22ce92
Eddie Belter <ebelter@gmail.com>
2019-12-30 11:37:17 -0600

    WIP Cohorts CLI

--- laims/cohorts_cli.py
+++ laims/cohorts_cli.py
@@ -1,7 +1,8 @@
 import click, os, tabulate, sys
+from sqlalchemy.orm.exc import NoResultFound
 
 from laims.app import LaimsApp
-#from laims.models import ComputeWorkflowSample, SampleCohort
+from laims.models import ComputeWorkflowSample, SampleCohort
 
 # COHORTS
 # create
@@ -15,9 +16,43 @@ def laims_cohorts_cli():
     """
     pass
 
+# [link]
+@click.command()
+@click.argument("name", type=str)
+@click.argument("samples", required=True, nargs=-1)
+def cohorts_link_cmd(name,samples):
+    """
+    Link samples to a cohort
+    """
+    sm = LaimsApp().db_connection()
+    session = sm()
+    if len(samples) == 1 and os.path.exists(samples[0]):
+        with open(samples[0], "r") as f:
+            names_to_link = set()
+            for l in f.readlines():
+                names_to_link.add( l.rstrip() )
+    else:
+        names_to_link = set(samples)
+    stats = { "add": 0, "skip": 0 }
+    for sample_name in names_to_link:
+        try:
+            sample = session.query(ComputeWorkflowSample).filter_by(ingest_sample_name=sample_name).one()
+        except NoResultFound:
+            raise Exception("Could not find sample named {}".format(sample_name))
+        cohort = session.query(SampleCohort).get((sample.id, name))
+        if cohort is None:
+            cohort = SampleCohort(name=name, sample_id=sample.id)
+            session.add(cohort)
+            stats["add"] += 1
+        else:
+            stats["skip"] += 1
+    session.commit()
+    sys.stderr.write("Added {} samples to cohort {}, skipped {} existing.".format(stats["add"], name, stats["skip"]))
+laims_cohorts_cli.add_command(cohorts_link_cmd, name="link")
+
 # [list]
 @click.command()
-def cohort_list_cmd():
+def cohorts_list_cmd():
     sm = LaimsApp().db_connection()
     session = sm()
     sql = "select name, count(*) as sample_count from sample_cohorts group by name"
@@ -26,4 +61,4 @@ def cohort_list_cmd():
     for cohort in result.fetchall():
         rows += [list(map(str, cohort))]
     sys.stdout.write( tabulate.tabulate(rows, ["NAME", "SAMPLE_COUNT"], tablefmt="simple") )
-laims_cohorts_cli.add_command(cohort_list_cmd, name="list")
+laims_cohorts_cli.add_command(cohorts_list_cmd, name="list")
---
+++ tests/test_cohorts_cli.py
@@ -0,0 +1,74 @@
+import os, shutil, tempfile, unittest
+from click.testing import CliRunner
+
+from laims.app import LaimsApp
+from laims.cohorts_cli import laims_cohorts_cli, cohorts_list_cmd, cohorts_link_cmd
+
+class LaimsCohortsCliTest(unittest.TestCase):
+    def setUp(self):
+        self.data_d = os.path.join(os.path.dirname(__file__), "data")
+
+        self.temp_d = tempfile.TemporaryDirectory()
+        self.database_fn = os.path.join(self.temp_d.name, "test.db")
+        shutil.copyfile(os.path.join(self.data_d, "test.db"), self.database_fn)
+
+        laimsapp = LaimsApp()
+        laimsapp.database = self.database_fn
+
+    def tearDown(self):
+        self.temp_d.cleanup()
+
+    def test1_laims_cohorts_cli(self):
+        runner = CliRunner()
+
+        result = runner.invoke(laims_cohorts_cli, [])
+        self.assertEqual(result.exit_code, 0)
+        result = runner.invoke(laims_cohorts_cli, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+
+    def test2_laims_cohorts_list_cmd(self):
+        runner = CliRunner()
+
+        result = runner.invoke(cohorts_list_cmd, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+
+        result = runner.invoke(cohorts_list_cmd)
+        try:
+            self.assertEqual(result.exit_code, 0)
+            expected_output = """NAME      SAMPLE_COUNT
+------  --------------
+afib                10"""
+            self.assertEqual(result.output, expected_output)
+        except:
+            print(result.output)
+            raise
+
+    def test3_laims_cohorts_link(self):
+        runner = CliRunner()
+
+        result = runner.invoke(cohorts_link_cmd, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+
+        samples = [ "H_XS-356091-0186761975", "H_XS-362472-0186761202", "H_WHEREAMI"]
+        new_crams = list(map(lambda s: os.path.join("/mnt/data/crams", s + ".cram"), samples))
+
+        with tempfile.NamedTemporaryFile(mode="w") as temp_f:
+            temp_f.write("\n".join(new_crams))
+            temp_f.flush()
+            result = runner.invoke(cohorts_link_cmd, ["NEW", temp_f.name])
+
+        try:
+            self.assertEqual(result.exit_code, 0)
+            new_crams = list(new_crams)
+            expected_output = "Added 2 samples to cohort , skipped 2 existing."
+            self.assertEqual(result.output, expected_output)
+        except:
+            print(result.output)
+            raise
+
+# -- LaimsCohortsCliTest
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
+#-- __main__
